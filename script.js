9f080e862a1358e9e471c75d98ba3267c07f93f6
Miro Zukina <92750661+MiroZukina@users.noreply.github.com>
2023-06-01 14:52:32 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -16,13 +16,21 @@ window.onscroll = () => {
       let offset = sec.offsetTop -100;
       let height= sec.offsetHeight;
       let id = sec.getAttribute('id');
-
+// active navbar
       if(top >= offset && top < offset + height){
         navLink.forEach(links => {
             links.classList.remove('active')
             document.querySelector('header nav a[href*=' + id +']').classList.add('active');
-        })
+        });
+// active anination on scroll
+sec.classList.add('show-animate');
 
+
+
+      }
+
+      else{
+         sec.classList.remove('show-animate');
       }
     });
 
@@ -35,3 +43,23 @@ window.onscroll = () => {
    menuIcon.classList.remove('bx-x');
    navbar.classList.remove('active');
 };
+
+// change color
+
+const randColor = () => {
+   return Math.floor(Math.random() * 256);
+ }
+ 
+ const button = document.querySelector('button');
+ button.addEventListener('click', colorChange);
+
+ const ps = document.querySelectorAll('p');
+  ps.forEach(p => {
+  p.addEventListener('click', colorChange);
+});
+ 
+ function colorChange() {
+   this.style.backgroundColor = `rgb(${randColor()}, ${randColor()}, ${randColor()})`;
+   this.style.color = `rgb(${randColor()}, ${randColor()}, ${randColor()})`;
+ }
+
