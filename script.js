65d2e873b2a105ef77177c67ef1a92fd54d2f151
Miro Zukina <92750661+MiroZukina@users.noreply.github.com>
2023-06-08 14:33:26 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -61,5 +61,116 @@ const randColor = () => {
  function colorChange() {
    this.style.backgroundColor = `rgb(${randColor()}, ${randColor()}, ${randColor()})`;
    this.style.color = `rgb(${randColor()}, ${randColor()}, ${randColor()})`;
- }
+ };
 
+ 
+
+  // Get the form element
+  const form1 = document.getElementById('form');
+  
+  // Attach event listener to form submit
+  form1.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+
+    // Get the input values
+    const name = document.getElementById('name').value;
+    const secondName = document.getElementById('secondName').value;
+    const email = document.getElementById('email').value;
+    const number = document.getElementById('number').value;
+    const message = document.getElementById('message').value;
+
+    const subject = 'Form Submission';
+    const body = `
+      Name: ${name}
+      Second Name: ${secondName}
+      Email: ${email}
+      Number: ${number}
+      Message: ${message}
+    `;
+
+    // Create the mailto link
+    const mailtoLink = `mailto:sschmiro@gmail.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+
+    // Open the user's email client
+    window.location.href = mailtoLink;
+  });
+
+
+  
+
+  const form = document.getElementById('form');
+const username = document.getElementById('name');
+const sName = document.getElementById('secondName');
+const email = document.getElementById('email');
+const number = document.getElementById('number');
+const text1 = document.getElementById('message');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText= message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+    const usernameValue = username.value.trim();
+    const sNameValue = sName.value.trim();
+    const emailValue = email.value.trim();
+    const numberValue = number.value.trim();
+    const text1Value= text1.value.trim();
+
+    if(usernameValue === '') {
+        setError(username, 'Name is required and cannot be left blank');
+    } else {
+        setSuccess(username);
+    }
+
+    if(sNameValue === '') {
+      setError(sName, 'Sacond Name is required and cannot be left blank');
+  } else {
+      setSuccess(sName);
+  }
+
+    if(emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address');
+    } else {
+        setSuccess(email);
+    }
+
+    if(numberValue === '') {
+        setError(number, 'Number is required');
+    } else {
+        setSuccess(number);
+    }
+
+    if(text1Value === '') {
+        setError(text1, 'Please confirm your text');
+    } else {
+        setSuccess(text1);
+    }
+
+};
